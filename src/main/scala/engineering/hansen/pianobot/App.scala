101ff2560b384ac0914fc17be284a6c708c39352
Robert J. Hansen <rjh@sixdemonbag.org>
2016-08-28 18:04:20 -0400

    Initial commit of skeleton + build env.

---
+++ src/main/scala/engineering/hansen/pianobot/App.scala
@@ -0,0 +1,11 @@
+package engineering.hansen.pianobot
+import java.sql.{Connection, DriverManager, ResultSet, SQLException, Statement}
+
+
+object App {
+  def main(args : Array[String]) {
+    Class.forName("org.sqlite.JDBC")
+    println( "Hello World!" )
+  }
+
+}
