55b50231f954a81f109fcc24b9002b2752cb81b1
Robert J. Hansen <rjh@sixdemonbag.org>
2016-09-01 11:10:54 -0400

    Not in a working state, but startup code is in place.

--- src/main/scala/engineering/hansen/pianobot/App.scala
+++ src/main/scala/engineering/hansen/pianobot/App.scala
@@ -5,7 +5,6 @@ import java.sql.{Connection, DriverManager, ResultSet, SQLException, Statement}
 object App {
   def main(args : Array[String]) {
     Class.forName("org.sqlite.JDBC")
-    engineering.hansen.pianobot.GetMusicDir.HW()
+    engineering.hansen.pianobot.Startup.HW()
   }
-
 }
