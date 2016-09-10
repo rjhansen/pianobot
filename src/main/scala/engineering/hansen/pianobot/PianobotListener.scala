9c48a71be61afcc101a3f3fd83233b9fdd2209f6
Robert J. Hansen <rjh@sixdemonbag.org>
2016-09-10 10:24:44 -0400

    Beginning to add IRC elements.

--- src/main/scala/engineering/hansen/pianobot/PianobotListener.scala
+++ src/main/scala/engineering/hansen/pianobot/PianobotListener.scala
@@ -8,29 +8,30 @@ import org.apache.logging.log4j.LogManager
 
 class PianobotListener extends ListenerAdapter {
   private val logger = LogManager.getLogger(getClass())
+  private val server = Environment.options("irc server")
 
   override def onConnect(e: ConnectEvent) = {
-    logger.info(s"""connected to ${Environment.get("irc server")}""")
+    logger.info(s"""connected to $server""")
   }
 
-  override def onConnectEventFailed(e: ConnectAttemptFailedEvent) = {
-    logger.fatal(s"""could not connect to ${Environment.get("irc server")}""")
+  override def onConnectAttemptFailed(e: ConnectAttemptFailedEvent) = {
+    logger.fatal(s"""could not connect to $server""")
     System.exit(0)
   }
 
   override def onDisconnect(e: DisconnectEvent) = {
-    logger.info(s"""disconnected from ${Environment.get("irc server")}""")
+    logger.info(s"""disconnected from $server""")
     logger.info("now terminating... hope you enjoyed Pianobot")
     System.exit(0)
   }
 
   override def onException(e: ExceptionEvent) = {
-    logger.fatal(s"exception: ${e.getException().toString}""")
-    logger.fatal(s"exception message: ${e.getMessage()}""")
+    logger.fatal(s"""exception: ${e.getException().toString}""")
+    logger.fatal(s"""exception message: ${e.getMessage()}""")
     System.exit(1)
   }
 
-  override def onNickAlreadyInUse(e: NickAlreadyInUse event) = {
+  override def onNickAlreadyInUse(e: NickAlreadyInUseEvent) = {
     logger.fatal(s"nick already in use.  Aborting.")
     System.exit(1)
   }
@@ -45,11 +46,13 @@ class PianobotListener extends ListenerAdapter {
     System.exit(1)
   }
 
-  override def onMessageEvent(e: MessageEvent) = {
+  override def onMessage(e: MessageEvent) = {
+    logger.debug("received message")
     // FIXME.  Lots of logic to go here!
   }
 
   override def onPrivateMessage(e: PrivateMessageEvent) = {
+    logger.debug("received private message")
     // FIXME.  Lots of logic to go here!
   }
 
@@ -61,8 +64,8 @@ class PianobotListener extends ListenerAdapter {
   override def onUserList(e: UserListEvent) = {
     val rx = "^([^!]+)!.*$".r
     SQLUtilities.sawPeople(
-      for (Some(i: String) <-
-        e.getUsers.toScala.flatMap((u: User) => u.getNick() match {
+      for (i: String <-
+        e.getUsers.iterator.asScala.toList.flatMap((u: org.pircbotx.User) => u.getNick() match {
           case rx(name) => Some(name)
           case _ => None
         })) yield i)
